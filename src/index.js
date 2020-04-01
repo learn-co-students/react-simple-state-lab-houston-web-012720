b38615aadc701a784c1bdafca0695e5e152ad0b4
anamsoomro <anamsoomroed@gmail.com>
2020-04-01 09:12:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,8 @@ import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
 
+
+
 ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
+// ReactDOM.render(<h1> Am I on the right page lmao</h1>, document.getElementById('root'));
+
